b1c09aec4cfaee37b7b3bd287c3692693adf37c3
Karl Nordenson <karl.nordenson@chasacademy.se>
2023-03-22 14:18:36 +0100

    Co-authored-by: Gustav Johansson <gustavnogood@users.noreply.github.com>

--- Jobchasers/src/App.jsx
+++ Jobchasers/src/App.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import companies from '../companies.json';
+import { setSelectedTag, setFilteredData} from './features/filterSlice';
+import store from './features/store'
 
 
 function App() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState('',[]);
+  
+  const dispatch = useDispatch();
+  
+  const selectedTag = useSelector((state) => state.filter.selectedTag);
   
   const search = (item) => {
     return (
@@ -12,13 +19,26 @@ function App() {
       item.category.join(' ').toLowerCase().includes(query.toLowerCase())
     );
   };
-
-const filteredData = companies.filter(search);
-
-
+ 
+  const handleTagClick = (tag) => {
+    dispatch(setSelectedTag(tag));
+  };
+  const filteredData = selectedTag
+    ? companies.filter((item) => item.category.includes(selectedTag))
+    : companies.filter(search);
+  dispatch(setFilteredData(filteredData));
+  console.log(store.getState())
+ 
   return (
     <div>
       <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
+      <div>
+        <button onClick={() => handleTagClick('Office')}>Office</button>
+        <button onClick={() => handleTagClick('Consulting')}>Consulting</button>
+        <button onClick={() => handleTagClick('Store')}>Store</button>
+        <button onClick={() => handleTagClick('Bank')}>Bank</button>
+        <button onClick={() => handleTagClick('Betting')}>Betting</button>
+      </div>
       <ul>
         {filteredData.map((item) => (
           <li key={item.id}>
@@ -30,5 +50,4 @@ const filteredData = companies.filter(search);
     </div>
   );
 }
-
 export default App;
---
+++ Jobchasers/src/features/filterSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+export const filterSlice = createSlice({
+  name: 'filter',
+  initialState: {
+    selectedTag: '',
+    filteredData: [],
+  },
+  reducers: {
+    setSelectedTag: (state, action) => {
+      state.selectedTag = action.payload;
+    },
+    setFilteredData: (state, action) => {
+      state.filteredData = action.payload;
+    },
+  },
+});
+export const { setSelectedTag, setFilteredData } = filterSlice.actions;
+export default filterSlice.reducer;
