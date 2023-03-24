56557833d489c4e62df9f9989e3fc6e082ad130e
Karl Nordenson <karl.nordenson@chasacademy.se>
2023-03-24 13:59:15 +0100

    Co-authored-by: Gustav Johansson <gustavnogood@users.noreply.github.com>

--- Jobchasers/src/App.jsx
+++ Jobchasers/src/App.jsx
@@ -1,39 +1,41 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import companies from '../companies.json';
-import { setSelectedTag, setFilteredData} from './features/filterSlice';
-import store from './features/store'
-
+import { setSelectedTag, setFilteredData } from './features/filterSlice';
 
 function App() {
   const [query, setQuery] = useState('');
   const dispatch = useDispatch();
-  const selectedTag = useSelector((state) => state.filter.selectedTag);
-  
-  const search = (item) => {
-    return (
-      item.companyName.toLowerCase().includes(query.toLowerCase()) ||
-      item.jobTitle.toLowerCase().includes(query.toLowerCase()) ||
-      item.category.join(' ').toLowerCase().includes(query.toLowerCase())
-    );
+  const { selectedTag, filteredData } = useSelector((state) => state.filter);
+
+
+  const filterData = (data, query, tag) => {
+    return data.filter((item) => {
+      const hasTag = !tag || item.category.includes(tag);
+      const hasQuery =
+        !query ||
+        item.companyName.toLowerCase().includes(query.toLowerCase()) ||
+        item.jobTitle.toLowerCase().includes(query.toLowerCase()) ||
+        item.category.join(' ').toLowerCase().includes(query.toLowerCase());
+      return hasTag && hasQuery;
+    });
   };
- 
+
   const handleTagClick = (tag) => {
     dispatch(setSelectedTag(tag));
-    const filteredData = companies.filter((item) => item.category.includes(tag));
+    const filteredData = filterData(companies, query, tag);
     dispatch(setFilteredData(filteredData));
   };
 
-  const filteredData = selectedTag
-    ? companies.filter((item) => item.category.includes(selectedTag))
-    : companies.filter(search);
-    console.log(store.getState());
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value);
+    const filteredData = filterData(companies, event.target.value, selectedTag);
+    dispatch(setFilteredData(filteredData));
+  };
 
-
- 
   return (
     <div>
-      <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
+      <input type="text" value={query} onChange={handleQueryChange} />
       <div>
         <button onClick={() => handleTagClick('Office')}>Office</button>
         <button onClick={() => handleTagClick('Consulting')}>Consulting</button>
@@ -52,4 +54,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
