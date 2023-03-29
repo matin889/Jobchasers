72e57cda93726637a6db9bcf0ee2b53d00aa6450
Karl Nordenson <karl.nordenson@chasacademy.se>
2023-03-29 13:16:17 +0200

    merged with css

--- Jobchasers/src/Components/JobList.jsx
+++ Jobchasers/src/Components/JobList.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 
 function JobList({ jobs }) {
   return (
+   <div> 
     <ul>
       {jobs.map((job) => (
-        <li key={job.id}>
-          <h2>{job.companyName}</h2>
-          <p>{job.jobTitle}</p>
-        </li>
+          <div className="job-card" key={job.id}>
+          <div className="company">
+            <a href={job.logoLink}>
+              <img className="logo" src={job.logo} alt="logo" />
+            </a>
+            <h2>{job.companyName}</h2>
+          </div>
+          <div className="job-details">
+            <h3 className="title">{job.jobTitle}</h3>
+            <p className="description">{job.jobDescription}</p>
+            <p>{job.typeOfEmployment}</p>
+            <p>{job.date}</p>
+          </div>
+        </div>
       ))}
     </ul>
+  </div>
   );
 }
 
--- Jobchasers/src/Components/SearchBar.jsx
+++ Jobchasers/src/Components/SearchBar.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-
+import style from '../../public/style.css'
 function SearchBar({ query, setQuery }) {
   const handleInputChange = (event) => {
     setQuery(event.target.value);
   };
 
   return (
+   <div>
+    <header></header>
     <input
+      className='search-box'
       type="text"
       placeholder="Search for jobs"
       value={query}
       onChange={handleInputChange}
     />
+    </div> 
   );
 }
 
