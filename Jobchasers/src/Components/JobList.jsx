a8510a74397884248a00eb9e2dadbae379a5b287
Md Abdul Matin <48449050+matin889@users.noreply.github.com>
2023-04-04 10:29:49 +0200

    Update JobList.jsx
    Added the attribute for opening the link in new tab

--- Jobchasers/src/Components/JobList.jsx
+++ Jobchasers/src/Components/JobList.jsx
@@ -7,11 +7,11 @@ function JobList({ jobs }) {
         {jobs.map((job) => (
           <div className="job-card" key={job.id}>
             <div className="company">
-              <a href={job.logoLink}>
+              <a href={job.logoLink} target="_blank">
                 <img className="logo" src={job.logo} alt="logo" />
               </a>
               <h2 className="company-name">
-                <a href={job.logoLink}>{job.companyName}</a>
+                <a href={job.logoLink} target="_blank">{job.companyName}</a>
               </h2>
             </div>
             <div className="job-details">
